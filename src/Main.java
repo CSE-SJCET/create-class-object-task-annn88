0a6a482ea347f70399a117e3ed53fb3eb013b109
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2025-07-22 22:41:58 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-class Student{
-    String name;
-    int rollNumber;
-    char grade;
-public void displayDetails() {
-    System.out.println("Name: "+name);
-    System.out.println("Roll Number: "+rollNumber);
-    System.out.println("Grade: "+grade);
+class Student {
+    private String name;
+    private int rollNumber;
+    private char grade;
+
+    public void setDetails(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
 }
-}   
- public class Main {
-    public static void main(String[] args) {
-        Student student=new Student();
-        student.name="Ann";
-        student.rollNumber=22;
-        student.grade='A';
-        student.displayDetails();
-   }
-}
+
